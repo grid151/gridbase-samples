815482974621984aa6ca5e7ab94348fe1b03349f
Octavio Gomez <93935918+tavigomez@users.noreply.github.com>
2022-12-01 19:42:09 -0500

    updated document webhook model

--- csharp/webhooks/aspdotnet6/Models/OrderDocumentData.cs
+++ csharp/webhooks/aspdotnet6/Models/OrderDocumentData.cs
@@ -7,16 +7,6 @@ public class OrderDocumentAddedData
 {
     /// <summary>GridBase Order ID</summary>
     public string OrderId { get; set; } = string.Empty;
-
-    /// <summary>Client's file number</summary>
-    public string ClientFileNumber { get; set; } = string.Empty;
-
-    /// <summary>Notes subject</summary>
-    public string? NoteSubject { get; set; }
-
-    /// <summary>Notes body</summary>
-    public string? NoteBody { get; set; }
-
     /// <summary>Documents array</summary>
     public List<DocumentData>? Documents { get; set; }
 }
@@ -26,16 +16,6 @@ public class DocumentData
 {
     /// <summary>GridBase document id</summary>
     public string? Id { get; set; }
-    /// <summary>Document type ID.</summary>
-    public string? DocumentTypeID { get; set; }
-    /// <summary>Document name, results in file name.</summary>
-    public string? DocumentName { get; set; }
-    /// <summary>Document reason for upload id.</summary>
-    public string? DocumentType { get; set; }
-    /// <summary>Date time document was added.</summary>
-    public DateTime? CreatedDate { get; set; }
-    /// <summary>Description</summary>
-    public string? Description { get; set; }
     /// <summary>File name.</summary>
     public string? FileName { get; set; }
 }
